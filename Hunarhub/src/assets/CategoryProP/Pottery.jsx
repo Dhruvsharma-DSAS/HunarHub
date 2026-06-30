9e0e39173d9613663633c7fcbae989e83f5ced77
Dhruv Sharmaa <dhruvsharma22052006@gmai.com>
2026-06-30 11:29:43 +0530

    Category Prop Add

---
+++ Hunarhub/src/assets/CategoryProP/Pottery.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import CategoriesBox from '../Component /CategoriesBox'
+
+
+
+const Pottery = () => {
+  return (
+
+    <CategoriesBox cat = "Potter (Kumhar)"/>
+
+  )
+}
+
+export default Pottery
---
+++ Hunarhub/src/assets/CategoryProP/Tailor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoriesBox from '../Component /CategoriesBox'
+
+const Tailor = () => {
+  return (
+
+    <CategoriesBox cat = "Tailor"/>
+
+  )
+}
+
+export default Tailor
